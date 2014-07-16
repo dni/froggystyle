1dfb17991e890968d389949578ce34d1965912e1
drsdrs <ae@dnilabs.com>
2014-07-16 18:36:24 +0200

    push!!!!!!!!!!!

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1024, 768, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
@@ -34,38 +34,39 @@ function stickToIt(pl, wall){
 	player.body.setZeroForce();
 	player.body.setZeroRotation();
 	game.physics.p2.gravity.y = 0;
-}
+};
 
 function die(){
+	resetLevel();
 	// die
 	c.l("dead");
 }
+var resetLevel = function(){
+	player.body.x = 10;
+	player.body.y = 10;
+};
 
-function setJumps(){ jumpsEl.innerHTML = jumps; };
+function setJumps(){ jumpsEl.innerHTML = jumps; }
 
 function create() {
-	// INIT AND SET STATS
 	jumpsEl = document.getElementById("jumps");
 	setJumps();
 
-    game.physics.startSystem(Phaser.Physics.P2JS);
     bg = game.add.tileSprite(0, 0, 1920, 768, 'bg');
     fg = game.add.tileSprite(0, 0, 1920, 768, 'fg');
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
 	game.physics.p2.setImpactEvents(true);
-
-
-    map = game.add.tilemap('desert');
-    map.addTilesetImage('background', 'bg');
-
     game.physics.p2.gravity.y = 600;
     game.physics.p2.restitution = 1;
 
+    map = game.add.tilemap('desert');
 
  	layer = map.createLayer('tileEbene');
  	layer.resizeWorld();
 
- 	startLayer = map.createLayer('start');
- 	goalLayer = map.createLayer('goal');
+ 	var startLayer = game.physics.p2.convertCollisionObjects(map, 'start')[0];
+ 	var goalLayer = game.physics.p2.convertCollisionObjects(map, 'goal')[0];
 
 	wallsCG = game.physics.p2.createCollisionGroup();
 	playerCG = game.physics.p2.createCollisionGroup();
@@ -85,54 +86,64 @@ function create() {
 	}
 
 	// INIT PLAYER
-    player = game.add.sprite(197, 165, 'frog');
+    player = game.add.sprite(133, 100, 'frog');
+    player.inputEnabled = true;
     game.physics.p2.enable(player);
     player.body.setCircle(28);
 	player.body.damping = 0.7
-
+	player.body.x = startLayer.x
+	player.body.y = startLayer.y
     player.body.setCollisionGroup(playerCG);
-
     player.body.collides(enemyCG, die, this);
-
     player.body.collides(wallsCG, stickToIt);
 
-    // player.body.setSize(20, 32, 5, 16);
-// player.body.fixedRotation = true;
 
+    goalFly = game.add.sprite(133, 100, 'frog');
+    window.gogo = goalLayer
+	goalFly.x = goalLayer.x-80;
+	goalFly.y = goalLayer.y-80;
+
+	// ANIMATIONS
     jump = player.animations.add('jump', [9,0,1,2,3], 10, false);
     landing = player.animations.add('landing', [9, 10], 10, false);
     player.animations.add('turn', [10], 2, true);
     fly = player.animations.add('fly', [4], 10, true);
 
 	landing.onComplete.add(function(){ player.play("sit"); }, this);
-	jump.onComplete.add(function(){ player.play("fly"); }, this);
 
+	jump.onComplete.add(function() { player.play("fly");	}, this);
 
-
-    game.camera.follow(player);
-
-	game.input.onDown.add(launch, this);
+	game.camera.follow(player);
+	player.events.onInputDown.add(startJump, this);
+	player.events.onInputUp.add(launch, this);
 }
-
+function startJump(){
+	player.play("jump");
+	setTimeout(function(){
+		launch();
+	}, 400);
+}
 function launch() {
-	if(flying===true||jumps===0){return false;}
+	if (flying === true || jumps === 0) { return false;	}
 	flying = true;
 	jumps--;
 	setJumps();
 	game.physics.p2.gravity.y = 600;
 	diffX = game.input.worldX - player.x;
 	diffY = game.input.worldY - player.y;
+
+	if (diffY > 0){ diffY = 0; }
+
 	veloX = diffX * 1.2;
-	veloY = diffY * 4;
-	player.play("jump");
-	if (diffX>0) {
+	veloY = diffY * 3;
+
+	if (diffX > 0) {
 		// jump right clicked
-		if (player.scale.x < 0) player.scale.x *= -1
+		if (player.scale.x < 0){ player.scale.x *= -1; }
 		player.body.velocity.x = veloX;
 		player.body.velocity.y = veloY;
-	}
-	else {
-		if (player.scale.x > 0) player.scale.x *= -1;
+	} else {
+		if (player.scale.x > 0){ player.scale.x *= -1; }
 		player.body.velocity.x = veloX;
 		player.body.velocity.y = veloY;
 	}
@@ -142,6 +153,6 @@ function update() {
 
 }
 
-function render () {
-    game.debug.body(player);
+function render() {
+	game.debug.body(player);
 }
