a487986a125b97e5fe21f01c472873a2a66372ff
drsdrs <ae@dnilabs.com>
2014-07-16 08:01:41 +0200

    port change

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(1667, function() {
     console.log('Listening on port %d', server.address().port);
 });
